ecb8cdcd049e353030f6be4cf8264f50f4848261
sudip <paudelsudip134@gmail.com>
2024-05-01 16:33:26 +0545

    use of static files -public files

--- express_js/main.js
+++ express_js/main.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.static('public'))//for creating public accessible files:http://localhost:3000/hello.txt
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
